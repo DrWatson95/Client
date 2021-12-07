3c5dbac68cd14f42f3c4e96ac4cba422b6879124
Iwan <iwan@iwan.iwan>
2021-12-07 08:45:28 +0500

    del debug inf

--- main.cpp
+++ main.cpp
@@ -1,17 +1,10 @@
 #include "myclient.h"
 #include <QApplication>
 
-
-void f(){
-    QList<QHostAddress> list = QNetworkInterface::allAddresses();
-    qDebug() << list[2].toString();
-}
-
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MyClient client("192.168.0.141",2323);
-    f();
     client.show();
 
     return a.exec();
